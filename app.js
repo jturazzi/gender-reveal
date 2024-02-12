6414cf2e180c7e83d655707c2b9677df3b067e2d
Jérémy Turazzi <jeremy@trz.fr>
2024-02-12 16:41:36 +0100

    Fix Bug

--- app.js
+++ app.js
@@ -10,15 +10,19 @@ app.use(express.static(`${__dirname}/public`));
 
 const boy = { color: '#BAE1FF', message: "C'est un garçon !" };
 const girl = { color: '#FFB0F8', message: "C'est une fille !" };
+
 const infos = {};
 
 function ResetInfos() {
   // Resetting the information object
-  infos.color = 'white';
-  infos.message = 'En attente de la révélation...';
-  infos.time = 31;
   infos.active = false;
-  io.emit('refresh', infos);
+  infos.time = 1;
+  setTimeout(() => {
+    infos.color = 'white';
+    infos.message = 'En attente de la révélation...';
+    io.emit('refresh', infos);
+  }, 1500);
+  
 }
 
 ResetInfos();
@@ -32,40 +36,57 @@ io.on('connection', (socket) => {
     io.emit('refresh', infos);
   });
 
+  socket.on('time', (data) => {
+    infos.time = data;
+    console.log(data);
+  });
+
   socket.on('admin', (data) => {
     console.log(data);
+    if (data === 'reset') { ResetInfos(); };
     if (infos.active === false) {
-      if (data === 'reset') { ResetInfos(); };
-
       if (data === 'boy' || data === 'girl') {
         infos.active = true;
 
         // Countdown
         const interval = setInterval(() => {
           infos.time -= 1;
-          if (infos.time > 60) {
+          if (infos.time > 15000) {
+            infos.message = `Révélation dans ${Math.floor(infos.time / 3600)} heures ${Math.floor((infos.time % 3600) / 60)} minutes`;
+            console.log(`Révélation dans ${Math.floor(infos.time / 60)} minutes`)
+          } else if (infos.time > 3600) {
+            infos.message = `Révélation dans ${Math.floor(infos.time / 60)} minutes`;
+            console.log(`Révélation dans ${Math.floor(infos.time / 60)} minutes`);
+          } else if (infos.time > 300) {
             infos.message = `Révélation dans ${Math.floor(infos.time / 60)} minutes ${(infos.time % 60)} secondes`;
             console.log(`Révélation dans ${Math.floor(infos.time / 60)} minutes ${(infos.time % 60)} secondes`);
-          } else {
+          } else if (infos.time > 10) {
             infos.message = `Révélation dans ${infos.time} secondes`;
             console.log(`Révélation dans ${infos.time} secondes`);
+          } else {
+            infos.message = `${infos.time}`;
+            console.log(`${infos.time}`);
           }
+          if (infos.active === true) {
           io.emit('countdown', infos);
-          if (infos.time === 0) {
-            clearInterval(interval);
-
-            if (data === 'boy') {
-              // Setting information for boy gender
-              infos.color = boy.color;
-              infos.message = boy.message;
-            } else if (data === 'girl') {
-              // Setting information for girl gender
-              infos.color = girl.color;
-              infos.message = girl.message;
+            if (infos.time === 0) {
+              clearInterval(interval);
+            
+              if (data === 'boy') {
+                // Setting information for boy gender
+                infos.color = boy.color;
+                infos.message = boy.message;
+              } else if (data === 'girl') {
+                // Setting information for girl gender
+                infos.color = girl.color;
+                infos.message = girl.message;
+              }
+              io.emit('refresh', infos);
+              infos.active = false;
+              console.log(infos.message);
             }
-            io.emit('refresh', infos);
-            infos.active = false;
-            console.log(infos.message);
+          } else {
+            clearInterval(interval);
           }
         }, 1000);
       }
